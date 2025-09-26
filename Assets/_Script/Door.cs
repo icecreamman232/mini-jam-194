d25c5099acfa2823befcda41b28187d80197f98c
Stephen Ho <stephen.ho.232@gmail.com>
2025-09-26 09:44:45 +0700

    Adding door

--- Assets/_Script/Door.cs
+++ Assets/_Script/Door.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using UnityEngine;
 
 public class Door : MonoBehaviour
@@ -5,10 +6,16 @@ public class Door : MonoBehaviour
     [SerializeField] private Transform m_model;
     [SerializeField] private Collider2D m_collider;
     
+    [ContextMenu("Show Door")]
     private void ShowDoor()
     {
         m_model.gameObject.SetActive(true);
         m_model.localScale = Vector3.one * 0.5f;
-        //m_model.
+        m_model.DOScale(Vector3.one,0.5f)
+            .SetEase(Ease.InOutQuint)
+            .OnComplete(() =>
+            {
+                m_collider.enabled = true;
+            });
     }
 }
