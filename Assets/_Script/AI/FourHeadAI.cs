00772729286111dbb0fa908666c920b4aed8fe50
Stephen Ho <stephen.ho.232@gmail.com>
2025-10-03 12:18:25 +0700

    Update FourHeadAI.cs

--- Assets/_Script/AI/FourHeadAI.cs
+++ Assets/_Script/AI/FourHeadAI.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using SGGames.Script.Core;
 using UnityEngine;
 
@@ -5,12 +6,25 @@ public class FourHeadAI : EnemyAI
 {
     [SerializeField] private EnemyHealth m_health;
     [SerializeField] private EnemyMovement m_movement;
+    [SerializeField] private int m_frameUpdatePathFinding = 60;
     private Transform m_playerTransform;
     private Vector3 m_directionToPlayer;
+    //Path finding
+    private RoomPathStatus m_roomPathStatus;
+    private List<Vector2> m_path;
+    private int m_frameCounter;
+    private int m_currentPathIndex;
 
     private void Start()
     {
+        m_roomPathStatus = ServiceLocator.GetService<RoomPathStatus>();
         m_playerTransform = ServiceLocator.GetService<LevelManager>().Player;
+        // Register this enemy with the pathfinding system
+        if (m_roomPathStatus != null)
+        {
+            m_roomPathStatus.RegisterEnemy(transform);
+        }
+        m_path = m_roomPathStatus.FindPathToPlayer(transform.position);
     }
     
     private void Update()
@@ -18,12 +32,35 @@ public class FourHeadAI : EnemyAI
         if (!this.gameObject.activeSelf) return;
         if (!m_canThink) return;
         if(m_playerTransform == null) return;
-
-        var lastestDirectionToPlayer = (m_playerTransform.position - transform.position).normalized;
-        if (m_directionToPlayer != lastestDirectionToPlayer)
+        
+        //Only update path every X frames
+        m_frameCounter++;
+        if (m_frameCounter >= m_frameUpdatePathFinding)
         {
-            m_directionToPlayer = lastestDirectionToPlayer;
-            m_movement.ChangeMoveDirection(lastestDirectionToPlayer);
+            m_path = m_roomPathStatus.FindPathToPlayer(transform.position);
+            m_frameCounter = 0;
+            m_currentPathIndex = 0;
+        }
+
+        if (m_path != null && m_path.Count != 0)
+        {
+            //Move to next point in path
+            if (Vector2.Distance((Vector3)m_path[m_currentPathIndex], transform.position) <= 0.1f)
+            {
+                m_currentPathIndex++;
+                if (m_currentPathIndex >= m_path.Count)
+                {
+                    m_path = m_roomPathStatus.FindPathToPlayer(transform.position);
+                    m_currentPathIndex = 0;
+                }
+            }   
+        }
+                     
+        var newDirectionToPlayer = ((Vector3)m_path[m_currentPathIndex] - transform.position).normalized;
+        if (newDirectionToPlayer != m_directionToPlayer)
+        {
+            m_directionToPlayer = newDirectionToPlayer;
+            m_movement.ChangeMoveDirection(m_directionToPlayer);
         }
     }
 }
